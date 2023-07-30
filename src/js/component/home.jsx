67578b28cf3f5e055ea0f659122eb8c5aeacaa42
Rick Rodriguez <rickrod6502@gmail.com>
2023-07-30 04:24:52 +0000

    created a controlled input example in Home component

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [input, setInput] = useState("");
+
+	function handleInput() {
+		if (input === "") {
+			alert("Input cannot be empty!")
+		}
+	}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<h1>Value you are inputting is: {input}</h1>
+			<input
+				type="text"
+				onChange={(event) => setInput(event.target.value)}
+				value={input}
+			/>
+			<button onClick={handleInput}
+				// onClick={() => {
+				// 	if(input === "") alert ("Input cannot be empty!")
+				// }}
+			>Validate</button>
+		</>
 	);
 };
 
